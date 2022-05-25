742a00cef45f606e7d3f9710aa9f505fcc3d6c20
Dayanne Alcantara <dayannealcantara14@gmail.com>
2022-05-25 10:39:14 -0300

    feat: criando scroll suave de navefação interna

--- script.js
+++ script.js
@@ -38,3 +38,23 @@ function initialAccordion() {
   }
 }
 initialAccordion();
+
+function scrollSuave() {
+  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+  function scrollToSection(e) {
+    e.preventDefault();
+    const href = e.currentTarget.getAttribute('href');
+    const section = document.querySelector(href);
+    const topo = section.offsetTop;
+
+    window.scrollTo({
+      top: topo,
+      behavior: 'smooth',
+    });
+  }
+
+  linksInternos.forEach((link) => {
+    link.addEventListener('click', scrollToSection);
+  });
+}
+scrollSuave();
